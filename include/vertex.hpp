a8cfa02635fe9b0478f2c2d7d223cd170784047d
wangxiangdong <xiangdong@icbench.com>
2023-01-26 22:35:59 +0800

    add vertex and main

--- include/vertex.hpp
+++ include/vertex.hpp
@@ -2,7 +2,7 @@
 #include <unordered_map>
 #include <vector>
 
-namespace xiangdongSTA {
+namespace STA {
 
 typedef std::string Cellname;
 typedef std::string Vertexname;
@@ -13,7 +13,7 @@ typedef double Require;
 typedef double Slack;
 typedef double Tclock;
 
-const int INF = 1E9;
+const double INF = 1E9;
 
 struct PinEdge {
   Pinname pfrom, pto;
@@ -27,8 +27,8 @@ class Cell {
   Cellname name() const { return name_; }
   std::vector<PinEdge*> pin_edges() const { return pin_edges_; }
   Delay getDelay(Pinname p) const {
-    for(PinEdge* pedge: pin_edges_) {
-      if(pedge->pfrom == p) return pedge->pdelay;
+    for (PinEdge* pedge : pin_edges_) {
+      if (pedge->pfrom == p) return pedge->pdelay;
     }
     return 0;
   }
@@ -38,17 +38,13 @@ class Cell {
   std::vector<PinEdge*> pin_edges_;
 };
 
-std::unordered_map<Cellname, Cell*> cell;
-
-struct NextVertexPin {
+struct VertexPin {
   Vertexname vname;
   Pinname pname;
 };
 
-struct PreVertexPin {
-  Vertexname vname;
-  Pinname pname;
-};
+typedef VertexPin NextVertexPin;
+typedef VertexPin PreVertexPin;
 
 class Vertex {
  public:
@@ -80,86 +76,116 @@ class Vertex {
   std::vector<PreVertexPin*> pres_;
 };
 
-std::unordered_map<Vertexname, Vertex*> vertex;
-
 class Edge {
  public:
-  Edge(const Vertex& vf, const Vertex& vt, const Pinname& pf, const Pinname& pt)
+  Edge(const Vertexname& vf, const Vertexname& vt, const Pinname& pf, const Pinname& pt)
       : vfrom_(vf), vto_(vt), pfrom_(pf), pto_(pt) {}
-  Vertex vfrom() const { return vfrom_; }
-  Vertex vto() const { return vto_; }
+  Vertexname vfrom() const { return vfrom_; }
+  Vertexname vto() const { return vto_; }
   Pinname pfrom() const { return pfrom_; }
   Pinname pto() const { return pto_; }
 
  protected:
-  Vertex vfrom_, vto_;
+  Vertexname vfrom_, vto_;
   Pinname pfrom_, pto_;
 };
 
 class Graph {
  public:
-  Graph(const Tclock& t, const std::vector<Vertex*>& v,
-        const std::vector<Edge*>& e)
-      : t_(t), vertexs_(v), edges_(e) {
+  Graph(const Tclock& t, const std::vector<Cell*>& c,
+        const std::vector<Vertex*>& v, const std::vector<Edge*>& e)
+      : t_(t), cells_(c), vertexs_(v), edges_(e) {
     init();
   }
   void init() {
     for (Vertex* v : vertexs_) {
       if (!v->pres().size()) starts_.push_back(v);
       if (!v->nexts().size()) ends_.push_back(v);
+      vertex_.insert({v->name(), v});
+    }
+    for(Cell* c : cells_) {
+      cell_.insert({c->name(), c});
+    }
+    for(Edge* e : edges_) {
+      Vertexname vf = e->vfrom(), vt = e->vto();
+      Pinname pf = e->pfrom(), pt = e->pto();
+      NextVertexPin nvp = {pf, pt};
+      vertex_[vf]->addnexts(&nvp);
+      PreVertexPin pvp = {vf, vt};
+      vertex_[pf]->addpres(&pvp);
     }
   }
   Delay pinLen(Vertex* v, Pinname p) {
-    if(!p.size()) return 0;
-    Cell* vcell = cell[v->type()];
+    if (!p.size()) return 0;
+    Cell* vcell = cell_[v->type()];
     return vcell->getDelay(p);
   }
   void dfs_next(Vertex* v) {
-    if(v->require() != INF) return ;
-    if(!v->nexts().size()) {
+    if (v->require() != INF) return;
+    if (!v->nexts().size()) {
       v->setrequire(t_);
-      return ;
+      return;
     }
     Require requireMin = INF;
-    for(NextVertexPin* nextvp : v->nexts()) {
-      Vertex* nextv = vertex[nextvp->vname];
+    for (NextVertexPin* nextvp : v->nexts()) {
+      Vertex* nextv = vertex_[nextvp->vname];
       Pinname nextp = nextvp->pname;
       dfs_next(nextv);
-      requireMin = std::min(requireMin, nextv->require() - pinLen(nextv, nextp));
+      requireMin =
+          std::min(requireMin, nextv->require() - pinLen(nextv, nextp));
     }
+    v->setrequire(requireMin);
   }
   void dfs_pre(Vertex* v) {
-    if (v->arrival() != INF) return ;
-    if(!v->pres().size()) {
+    if (v->arrival() != INF) return;
+    if (!v->pres().size()) {
       v->setarrival(0);
-      return ;
+      return;
     }
     Arrival arrivalMax = -INF;
-    for(PreVertexPin* prevp : v->pres()) {
-      Vertex* prev = vertex[prevp->vname];
+    for (PreVertexPin* prevp : v->pres()) {
+      Vertex* prev = vertex_[prevp->vname];
       Pinname prep = prevp->pname;
       dfs_pre(prev);
-      arrivalMax = std::max(arrivalMax, prev->arrival() - pinLen(prev, prep));
+      arrivalMax = std::max(arrivalMax, prev->arrival() + pinLen(prev, prep));
     }
+    v->setarrival(arrivalMax);
   }
   void calcArrival() {
     for (Vertex* v : ends_) {
       dfs_pre(v);
     }
   }
-  void calcRequire(){
-    for(Vertex* v : starts_) {
+  void calcRequire() {
+    for (Vertex* v : starts_) {
       dfs_next(v);
     }
   }
-  void calcSlack();
+  void calcSlack() {
+    calcArrival();
+    calcRequire();
+    for (Vertex* v : vertexs_) {
+      v->setslack(v->require() - v->slack());
+    }
+  }
+  Tclock t() const { return t_; }
+  std::vector<Cell*> cells() const { return cells_; }
+  std::vector<Vertex*> vertexs() const { return vertexs_; }
+  std::vector<Edge*> edges() const { return edges_; }
+  std::vector<Vertex*> starts() const { return starts_; }
+  std::vector<Vertex*> ends() const { return ends_; }
+  std::unordered_map<Cellname, Cell*> cell() const { return cell_; }
+  std::unordered_map<Vertexname, Vertex*> vertex() const { return vertex_; }
 
  protected:
   Tclock t_;
+  std::vector<Cell*> cells_;
   std::vector<Vertex*> vertexs_;
+  std::vector<Edge*> edges_;
   std::vector<Vertex*> starts_;
   std::vector<Vertex*> ends_;
-  std::vector<Edge*> edges_;
+  std::unordered_map<Cellname, Cell*> cell_;
+  std::unordered_map<Vertexname, Vertex*> vertex_;
 };
 
 }  // namespace xiangdongSTA
---
+++ src/main.cpp
@@ -0,0 +1,106 @@
+#include <fstream>
+#include <iostream>
+
+#include "vertex.hpp"
+
+namespace STA {
+
+extern const double INF;
+
+Tclock readSdc() {
+  std::ifstream ifs;
+  ifs.open("../sdc/adder.fakesdc", std::ios::in);
+  if (!ifs.is_open()) {
+    return 0;
+  }
+  std::string str;
+  double t;
+  ifs >> str >> t;
+  return t;
+}
+
+void readLib(std::vector<Cell*>& cells) {
+  std::ifstream ifs;
+  ifs.open("../lib/tsmc65.fakelib", std::ios::in);
+  if (!ifs.is_open()) {
+    return;
+  }
+  int n;
+  ifs >> n;
+  while (n--) {
+    std::string str;
+    int m;
+    ifs >> str >> m;
+    std::vector<PinEdge*> pinedges;
+    while (m--) {
+      std::string in_pin, out_pin, dis;
+      ifs >> in_pin >> out_pin >> dis;
+      Delay d = INF;
+      if (dis != "INF") d = stod(dis);
+      PinEdge pe = PinEdge{in_pin, out_pin, d};
+      pinedges.push_back(&pe);
+    }
+    Cell cell = Cell(str, pinedges);
+    cells.push_back(&cell);
+  }
+}
+
+void findCellPin(const std::string& str, std::string& cell, std::string& pin) {
+  int pos = str.find('/');
+  if (pos == -1) {
+    cell = str;
+    pin = "";
+  } else {
+    cell = str.substr(0, pos);
+    pin = str.substr(pos + 1);
+  }
+}
+
+void readNetlists(std::vector<Vertex*>& vertexs, std::vector<Edge*>& edges) {
+  std::ifstream ifs;
+  ifs.open("../verilog/adder.fakev", std::ios::in);
+  if (!ifs.is_open()) {
+    return;
+  }
+  int n;
+  ifs >> n;
+  while (n--) {
+    std::string name, type;
+    ifs >> name >> type;
+    Vertex v = Vertex(name, type);
+    vertexs.push_back(&v);
+  }
+  int m;
+  ifs >> m;
+  while (m--) {
+    std::string from, to;
+    ifs >> from >> to;
+    std::string from_cell, from_pin, to_cell, to_pin;
+    findCellPin(from, from_cell, from_pin);
+    findCellPin(to, to_cell, to_pin);
+    Edge edge = Edge(from_cell, to_cell, from_pin, to_pin);
+    edges.push_back(&edge);
+  }
+}
+
+int main() {
+    Tclock t = readSdc();
+    std::vector<Cell*> cells;
+    readLib(cells);
+    std::vector<Vertex*> vertexs;
+    std::vector<Edge*> edges;
+    readNetlists(vertexs, edges);
+    Graph graph = Graph(t, cells, vertexs, edges);
+    graph.calcSlack();
+    using namespace std;
+    cout << "t: " << graph.t() << endl;
+    cout << "starts:" << endl;
+    for(Vertex* v : graph.starts()) cout << v->name() << " ";
+    cout << endl;
+    cout << "ends:" << endl;
+    for(Vertex* v : graph.ends()) cout << v->name() << " ";
+    cout << endl;
+    return 0;
+}
+
+}  // namespace xiangdongSTA
